6c3a95022532f591f7cdb4d8541d13b8cfbc7160
L1LB3AR <achozas19@icloud.com>
2021-10-01 20:32:10 +0200

    controller

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-shadow */
 const mongoose = require('mongoose');
 
-const Books = mongoose.model('books');
+const Book = mongoose.model('Books');
 
 // GET - Return all the books
 exports.findAllbooks = function (req, res) {
-  Books.find((err, books) => {
+  Book.find((err, books) => {
     if (err) res.send(500, err.message);
 
     console.log('GET /books');
@@ -14,7 +18,7 @@ exports.findAllbooks = function (req, res) {
 
 // GET - Return a Book with specified ID
 exports.findById = function (req, res) {
-  Books.findById(req.params.id, (err, books) => {
+  Book.findById(req.params.id, (err, books) => {
     if (err) {
       return res.send(500, err.message);
     }
@@ -29,16 +33,40 @@ exports.addBook = function (req, res) {
   console.log('Post');
   console.log(req.body);
 
-  const booksmodel = new Books({
+  // eslint-disable-next-line no-use-before-define
+  const Book = new Book({
     title: req.body.title,
     pages: req.body.pages,
     genre: req.body.genre,
   });
 
-  booksmodel.save((err, books) => {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-    res.status(200).jsonp(books);
+  Book.save((err, book) => {
+    if (err) return res.status(500).send(err.message);
+
+    res.status(200).jsonp(book);
+  });
+};
+
+// PUT - Update a book already exists
+exports.updateBook = function (req, res) {
+  Book.findById(req.params.id, (err, books) => {
+    books.title = req.body.petId;
+    books.pages = req.body.pages;
+    books.genre = req.body.genre;
+
+    books.save((err) => {
+      if (err) return res.status(500).send(err.message);
+      res.status(200).jsonp(books);
+    });
+  });
+};
+
+// DELETE - Delete a Book with specified ID
+exports.deleteBook = function (req, res) {
+  Book.findById(req.params.id, (err, books) => {
+    books.remove((err) => {
+      if (err) return res.status(500).send(err.message);
+      res.status(200).send();
+    });
   });
 };
