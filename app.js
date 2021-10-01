d0c0bb9a77594b38a80868fb67e83e98b7322518
L1LB3AR <achozas19@icloud.com>
2021-10-01 20:34:43 +0200

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// Variables
 const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+const router = require('./router/userouter');
 
 const app = express(); // inicializacion
 
+// eslint-disable-next-line no-unused-vars
+const server = http.createServer(app);
+
 const Puerto = process.env.Puerto || 3000; // prepara el puerto
+
+mongoose.connect('mongodb://localhost/books');
+
+// Set API response
+router.get('/', (req, res) => {
+  res.json({
+    status: 'API Its Working',
+    message: 'Welcome to RESThub',
+  });
+});
 
 // methods
 
@@ -22,6 +39,39 @@ app.listen(Puerto, () => {
 
 const userouter = require('./router/userouter');
 
+app.use('/api', userouter);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', userouter);
+
+// Import books controller
+const BooksController = require('./controller/usercontroller');
+
+// Books routes
+const books = express.Router();
+
+books.route('books');
+books.get(BooksController.findAllbooks);
+books.post(BooksController.addBook);
+
+books.route('/books/:id');
+books.get(BooksController.findById);
+books.put(BooksController.updateBook);
+books.delete(BooksController.deleteBook);
+
+app.use('/api', books);
+
+// Connection mongoose
+// eslint-disable-next-line no-unused-vars
+mongoose.connect('mongodb://localhost/books', (err, res) => {
+  if (err) {
+    console.log(`ERROR: connecting to Database. ${err}`);
+  }
+  app.listen(3000, () => {
+    console.log('Node server running on https://localhost:3000');
+  });
+});
+
+// Export API Routes
+module.exports = router;
